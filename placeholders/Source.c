5591613e8621bcad6a06a3430b1063a713e59205
arshb <arshb@LAPTOP-KGDE4GHJ>
2022-02-02 22:22:18 -0400

    Add project files.

---
+++ placeholders/Source.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("My name is %s\n", "Arsh"); // %s placeholder is for String 
+	printf("Char is %c\n", 'A'); // %c is placeholder for single character and the character is held in single quotation 
+	printf("My name is %s and my Initial is %c\n", "Arsh", 'A'); // %s and %c used in single line.
+
+	/*Now i am going to use PErcentage placeholder */
+
+	printf("My !2th grade percentage is %d%% \n", 70);
+	return(0);
+}
